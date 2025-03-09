2aeb95c3c4a86c5fcd36a9b956d1e367462087f4
austaras <austaras@outlook.com>
2025-03-09 12:44:23 +0800

    init test

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,7 @@ use std::path::PathBuf;
 
 use rocksdb::{DB, MergeOperands, Options};
 use yrs::{
-    Update,
-    updates::{decoder::Decode, encoder::Encode},
+    updates::{decoder::Decode, encoder::{Encode, Encoder, EncoderV1}}, Transact, Update
 };
 
 pub struct YRock {
@@ -12,18 +11,22 @@ pub struct YRock {
 
 fn reencode_merge(_: &[u8], prev: Option<&[u8]>, op: &MergeOperands) -> Option<Vec<u8>> {
     let mut update = if let Some(prev) = prev {
-        Update::decode_v1(prev).unwrap()
+        Update::decode_v1(prev).ok()?
     } else {
         Update::new()
     };
+    
 
     for op in op {
-        let next_update = Update::decode_v1(op).unwrap();
+        let next_update = Update::decode_v1(op).ok()?;
 
         update.merge(next_update);
     }
 
-    Some(update.encode_v1())
+    let mut encoder = EncoderV1::new();
+    update.encode(&mut encoder);
+
+    Some(encoder.to_vec())
 }
 
 impl YRock {
@@ -34,7 +37,7 @@ impl YRock {
         Ok(Self { inner: db })
     }
 
-    pub fn get(&self, key: &[u8]) -> Result<Option<Update>, rocksdb::Error> {
+    pub fn get(&self, key: &[u8]) -> Result<Option<yrs::Doc>, rocksdb::Error> {
         let value = self.inner.get(key)?;
 
         let Some(value) = value else {
@@ -43,7 +46,13 @@ impl YRock {
 
         let update = Update::decode_v1(&value).expect("invalid stored data");
 
-        Ok(Some(update))
+        let doc = yrs::Doc::new();
+
+        let mut trx = doc.transact_mut();
+        trx.apply_update(update).expect("invalid stored data");
+        drop(trx);
+
+        Ok(Some(doc))
     }
 
     pub fn store_update(&self, key: &[u8], update: Update) -> Result<(), rocksdb::Error> {
---
+++ tests/test.rs
@@ -0,0 +1,38 @@
+use yrock::YRock;
+use yrs::{Doc, GetString, Text, Transact, Update, updates::decoder::Decode};
+
+#[test]
+fn update_encoding() {
+    let doc = Doc::new();
+
+    let text = doc.get_or_insert_text("test");
+
+    let upd = {
+        let mut trx = doc.transact_mut();
+
+        text.insert(&mut trx, 0, "abc");
+        let len = text.len(&trx);
+        text.insert(&mut trx, len, "def");
+
+        let upd = trx.encode_update_v1();
+
+        trx.commit();
+
+        upd
+    };
+
+    let new_doc = Doc::new();
+
+    {
+        let mut trx = new_doc.transact_mut();
+
+        trx.apply_update(Update::decode_v1(&upd).unwrap()).unwrap();
+
+        trx.commit();
+    }
+
+    let text = new_doc.get_or_insert_text("test");
+    let content = text.get_string(&new_doc.transact());
+
+    assert_eq!(content, "abcdef")
+}
